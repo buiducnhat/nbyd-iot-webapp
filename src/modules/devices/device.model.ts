90761832b1d8abeb1dbf1d6aba9b86527183897f
Nhat Bui <nhaths4701@gmail.com>
2024-04-11 08:23:11 +0700

    feat: add device type: pi

--- src/modules/devices/device.model.ts
+++ src/modules/devices/device.model.ts
@@ -3,11 +3,13 @@ import { TDatastream } from '../datastreams/datastream.model';
 export enum EDeviceHardware {
   ESP32 = 'ESP32',
   ESP8266 = 'ESP8266',
+  RASPBERRY_PI = 'RASPBERRY_PI',
 }
 
 export const EDeviceHardwareOptions = [
   { label: 'ESP32', value: EDeviceHardware.ESP32 },
   { label: 'ESP8266', value: EDeviceHardware.ESP8266 },
+  { label: 'RASPBERRY PI', value: EDeviceHardware.RASPBERRY_PI },
 ];
 
 export enum EDeviceStatus {
