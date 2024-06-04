827f52ed399e4aa2a30562332f8a106d9bb4ffd2
Nhat Bui <nhaths4701@gmail.com>
2024-06-04 10:10:14 +0700

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,43 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
     }),
     TanStackRouterVite(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'NBYD IoT', // The name of your app
+        short_name: 'NBYD IoT', // The short name of your app
+        theme_color: '#fff', // The theme color of your app
+        background_color: '#fff', // The background color of your app
+        display: 'standalone', // The display mode of your app
+        scope: '/', // The scope of your app
+        start_url: '/', // The start URL of your app
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
